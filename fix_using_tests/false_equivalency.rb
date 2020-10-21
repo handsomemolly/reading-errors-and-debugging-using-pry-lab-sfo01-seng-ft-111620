4867281b56fdd20e694a9afdc069879129e8b3e8
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-21 17:09:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -4,7 +4,6 @@ def get_user_input
   gets.chomp
 end
 
-require 'pry'
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
@@ -13,10 +12,10 @@ end
 
 def selection(num)
   if num = 1
-    binding.pry
+    
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
-  binding.pry
+  
      "HAM HAM HAM IN MY TUMMY"
   end
 end
